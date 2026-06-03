6e1a42ca633e860a9a95c8ce3fdfd9e389657e50
lealmasa <itdepartment@mangonelawfirmllc.com>
2026-06-03 10:11:52 -0300

    Initial commit

---
+++ astro.config.mjs
@@ -0,0 +1,8 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://devautomaticn.github.io',
+  base: '/automatic-nation-website',
+});
